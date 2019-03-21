655ac2fa60d8b2738bbd2e8a75ac3222d3c575c1
Andrey <a.betin93@yandex.ru>
2019-03-21 15:29:07 +0300

    Добавил счётчик для корзины

--- js/buy.js
+++ js/buy.js
@@ -1,15 +1,26 @@
   var buyWindow = document.querySelectorAll(".buy");
   var modalBuy = document.querySelector(".added-product");
   var closeBuy = document.querySelector(".close-order");
+  var cancelBuy = modalBuy.querySelector(".continue-shopping");
+  var countBuy = document.querySelector(".cart")
+  var counter = countBuy.querySelector(".count");
 
   for (var i = 0; i < buyWindow.length; i++) {
       buyWindow[i].addEventListener("click", function(evt) {
         evt.preventDefault();
         modalBuy.classList.remove("visually-hidden");
+        countBuy.classList.add("filled");
+        var count = Number(counter.innerHTML);
+        counter.innerHTML = count += 1;
     });
   }
 
   closeBuy.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalBuy.classList.add("visually-hidden");
+  });
+
+  cancelBuy.addEventListener("click", function (evt) {
     evt.preventDefault();
     modalBuy.classList.add("visually-hidden");
   });
